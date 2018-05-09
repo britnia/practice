fc08589e833b44493e234d6643812e6ec864607c
Britni Alexander <britnialexander@gmail.com>
2018-05-09 09:14:09 -0400

    write tests and refactor yesterdays stuff

---
+++ lib/project-euler/even_fibonacci_numbers.rb
@@ -0,0 +1,11 @@
+def even_fibonacci_sum(limit)
+  limit
+end
+
+def fibonacci_numbers(limit)
+
+end
+
+def even_numbers(fibs)
+
+end
--- spec/project-euler/multiples_of_3_and_5_spec.rb
+++ spec/project-euler/multiples_of_3_and_5_spec.rb
@@ -1,11 +1,10 @@
-/ *If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-  The sum of these multiples is 23.
-  Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
 require_relative '../../lib/project-euler/multiples_of_3_and_5'
 
-describe 'threes_and_fives' do
+describe '#sum_threes_and_fives' do
   it 'gives the sum of all numbers under a given number that are divisible by 3 or 5' do
     expect(sum_threes_and_fives(10)).to eq(23)
   end
