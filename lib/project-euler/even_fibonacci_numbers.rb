14e86538acb0937dcfa0bcd127515a0f428a13e2
Britni Alexander <britnialexander@gmail.com>
2018-05-09 09:28:22 -0400

    generate fibbonacci numbers

--- lib/project-euler/even_fibonacci_numbers.rb
+++ lib/project-euler/even_fibonacci_numbers.rb
@@ -3,7 +3,15 @@ def even_fibonacci_sum(limit)
 end
 
 def fibonacci_numbers(limit)
-
+  x = 1
+  y = 0
+  fibs = []
+  until (x + y) > limit do
+    fibs.push(x + y)
+    y = x
+    x = fibs.last
+  end
+  fibs
 end
 
 def even_numbers(fibs)
